6d73a7d9866affc2860cc5293672811723186d16
Tolulope Oshinowo <toluooshy@gmail.com>
2021-11-03 13:31:44 -0400

    Preliminary order logic with state.

--- cup-noodle-app/src/App.js
+++ cup-noodle-app/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { indexOf } from "lodash";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [ramen, setRamen] = useState(() => {
+    return null;
+  });
+
+  const [toppings, setToppings] = useState(() => {
+    return [];
+  });
+
+  const updateRamen = (flavor) => {
+    setRamen((prevRamen) => flavor);
+  };
+
+  const updateToppings = (topping) => {
+    setToppings((prevToppings) =>
+      prevToppings.indexOf(topping) === -1
+        ? [...prevToppings, topping]
+        : prevToppings.filter((item) => item !== topping)
+    );
+  };
+
+  const submitOrder = (order) => {
+    order = [ramen, toppings];
+    console.log(order);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Ramen Order</h1>
+
+      <h3>Choose Ramen</h3>
+      <button onClick={() => updateRamen("Chicken")}>Chicken</button>
+      <br />
+      <button onClick={() => updateRamen("Beef")}>Beef</button>
+      <br />
+      <button onClick={() => updateRamen("Shrimp")}>Shrimp</button>
+      <br />
+      <button onClick={() => updateRamen("Pork")}>Pork</button>
+      <br />
+      <span>Your Ramen: {ramen}</span>
+
+      <h3>Choose Toppings</h3>
+      <button onClick={() => updateToppings("Onion")}>Onion</button>
+      <br />
+      <button onClick={() => updateToppings("Corn")}>Corn</button>
+      <br />
+      <button onClick={() => updateToppings("Mushroom")}>Mushroom</button>
+      <br />
+      <button onClick={() => updateToppings("Egg")}>Egg</button>
+      <br />
+      <span>Your Toppings: {JSON.stringify(toppings)}</span>
+
+      <br />
+      <br />
+      <button onClick={() => submitOrder()}>SUBMIT</button>
+    </>
   );
 }
 
