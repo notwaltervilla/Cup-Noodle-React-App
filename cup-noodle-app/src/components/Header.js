6c60e61d300ee02a2c095d6e6c21eb2c3078a70e
Walter Villa <notwaltervilla@gmail.com>
2021-11-17 20:24:40 -0500

    fixed typo in Header.js component - ready to merge

--- cup-noodle-app/src/components/Header.js
+++ cup-noodle-app/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div className='header'>
             <h3> Cup of Noodles - Vending Machine </h3>    
             <p>
-                Hello, welcome to our PIE Project website. In this web page, you will be able to get an overview of our process, and even be control what Ramen you are making!
+                Hello, welcome to our PIE Project website. In this web page, you will be able to get an overview of our process, and even control the type of ramen you are making!
             </p>
         </div>
     )
