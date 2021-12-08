6245f5d72f215332b672a871815db3aaf2ebe2d8
toluooshy <toluooshy@gmail.com>
2021-12-08 15:29:06 -0500

    Updated webapp.

--- cup-noodle-app/src/components/NavBar.js
+++ cup-noodle-app/src/components/NavBar.js
@@ -3,11 +3,27 @@ import UnstyledLink from "./UnstyledLink";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const style = {
+    color: "#ffffff",
+    backgroundColor: "#879107",
+    padding: "auto",
+    display: "flex",
+    justifyContent: "space-between",
+    fontSize: "20px",
+    boxShadow: "0px 5px #000000",
+  };
+
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/order">Order</Link>
-    </div>
+    <>
+      <div style={style}>
+        <div style={{ padding: "10px" }}>
+          <UnstyledLink url="/">Home</UnstyledLink>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <UnstyledLink url="/order">Order</UnstyledLink>
+        </div>
+      </div>
+    </>
   );
 }
 
--- cup-noodle-app/src/components/UnstyledLink.js
+++ cup-noodle-app/src/components/UnstyledLink.js
@@ -4,14 +4,25 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const UnstyledLink = ({ children, url }) => {
+  const [linkColor, setLinkColor] = useState("#ffffff");
+
   const style = {
     textDecoration: "none",
+    color: linkColor,
+  };
+  const target = `${url}`;
+
+  const updateLinkColor = (active) => {
+    active ? setLinkColor("#000000") : setLinkColor("#ffffff");
   };
 
-  const target = `/${url}`;
-
   return (
-    <Link style={style} to={target}>
+    <Link
+      style={style}
+      to={target}
+      onMouseEnter={() => updateLinkColor(true)}
+      onMouseLeave={() => updateLinkColor(false)}
+    >
       {children}
     </Link>
   );
